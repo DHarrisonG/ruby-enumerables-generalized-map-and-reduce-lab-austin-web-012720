af3c94b15cc93f2e8f058bfbffb3661abecb7879
Doug Harrison <dougharrisong@gmail.com>
2020-01-12 17:15:35 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,22 @@
-# Your Code Here
+array = [1,2,3]
+
+def map(arr)
+  new= []
+  i = 0
+  while i < arr.length do
+    new << yield(arr[i])
+    i += 1
+  end
+  new
+end
+
+def reduce(arr)
+  new = []
+  i = 0
+  while i < arr.length do
+    new << yield(arr[i])
+    i += 1
+  end
+  new
+end
+
